2a3e91eecbaa3853f001c7ae9bb0047c9bc57dc7
llekcevi <69637064+llekcevi@users.noreply.github.com>
2020-10-12 19:31:01 +0200

    Add files via upload
    Exercise 2 - Odd or Even with just the first extra.

---
+++ exercise-2.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("You have entered an even number.")
+else:
+    print("You have entered an odd number")
+if number % 4 == 0:
+    print("The number you have entered is a multiple of 4.")
