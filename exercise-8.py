29cb41d38f3c55988755b0ac89ed3dfa8ed77721
llekcevi <69637064+llekcevi@users.noreply.github.com>
2020-12-12 14:12:09 +0100

    Add files via upload

---
+++ exercise-8.py
@@ -0,0 +1,64 @@
+"""Make a two-player Rock-Paper-Scissors game. 
+(Hint: Ask for player plays (using input), compare them, 
+print out a message of congratulations to the winner, 
+and ask if the players want to start a new game)
+
+Remember the rules:
+
+    Rock beats scissors
+    Scissors beats paper
+    Paper beats rock"""
+
+print("\n ROCK PAPER SCISSORS \n ")
+
+#asks for players names
+player_name_1 = input("What is your name, player one? \n")
+player_name_2 = input("What is your name, player two? \n")
+
+#boolean for repeating the game
+again = True
+
+#while loop for repeating the game
+while again:
+    #variables hold each players move
+    player_1 = input(f"{player_name_1}, do you chose rock, paper or scissors? \n ")
+    player_2 = input(f"{player_name_2}, do you chose rock, paper or scissors? \n ")
+
+    #comparing strings the players have entered
+    if player_1 == player_2:
+        print("It's a draw.")
+
+    while player_1 == "rock":           
+        if player_2 == "paper":
+            print(f"{player_name_2} wins!")
+            break
+        elif player_2 == "scissors":
+            print(f"{player_name_1} wins!")
+            break
+
+    while player_1 == "paper":
+        if player_2 == "scissors":
+            print(f"{player_name_2} wins!")
+            break
+        elif player_2 == "rock":
+            print(f"{player_name_1} wins!")
+            break
+
+    while player_1 == "scissors":
+        if player_2 == "rock":
+            print(f"{player_name_2} wins!")
+            break
+        elif player_2 == "paper":
+            print(f"{player_name_1} wins!")
+            break
+    repeat = input("Do you want to play again? (Type yes or no.) \n")
+    if repeat == "no":
+        again = False
+        print("Game ended.")
+
+
+
+
+
+
+
