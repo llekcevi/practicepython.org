f1a63b9b1d1ea2fd6ae40178eaa0e79c635e0754
llekcevi <69637064+llekcevi@users.noreply.github.com>
2020-12-12 19:50:29 +0100

    ROCK-PAPER-SCISSORS-3.PY
    Added a function play_game() that contains most of the rules for the game.

--- exercise-8.py
+++ exercise-8.py
@@ -8,18 +8,7 @@ Remember the rules:
     Rock beats scissors
     Scissors beats paper
     Paper beats rock"""
-
-print("\n ROCK PAPER SCISSORS \n ")
-
-#asks for players names
-player_name_1 = input("What is your name, player one? \n")
-player_name_2 = input("What is your name, player two? \n")
-
-#boolean for repeating the game
-again = True
-
-#while loop for repeating the game
-while again:
+def play_game():  
     #variables hold each players move
     player_1 = input(f"{player_name_1}, do you chose rock, paper or scissors? \n ")
     player_2 = input(f"{player_name_2}, do you chose rock, paper or scissors? \n ")
@@ -51,10 +40,25 @@ while again:
             elif player_2 == "paper":
                 print(f"{player_name_1} wins!")
                 break
+    
+print("\n ROCK PAPER SCISSORS \n ")
+
+#asks for players names
+player_name_1 = input("What is your name, player one? \n")
+player_name_2 = input("What is your name, player two? \n")
+
+#boolean for repeating the game
+again = True
+#game
+play_game()
+#while loop for repeating the game
+while again:
     repeat = input("Do you want to play again? (Type yes or no.) \n")
     if repeat == "no":
         again = False
         print("Game ended.")
+    else:
+        play_game()
 
 
 
